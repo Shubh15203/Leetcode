2fcbede76b2edd14a6cba471be8e30f6cb30d8e2
Shubh15203 <146938449+Shubh15203@users.noreply.github.com>
2024-07-22 23:46:54 +0530

    Time: 105 ms (56.77%) | Memory: 17 MB (45.10%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,17 +1,3 @@
 class Solution:
-    def sortPeople(self, names: List[str], A: List[int]) -> List[str]:
-        for i in range(len(A)-1):
-    
-        # Find the minimum element in remaining 
-        # unsorted array
-            min_idx = i
-            for j in range(i+1, len(A)):
-                if A[min_idx] > A[j]:
-                    min_idx = j
-            
-        # Swap the found minimum element with 
-        # the first element        
-            A[i], A[min_idx] = A[min_idx], A[i]
-            names[i], names[min_idx] = names[min_idx], names[i]
-
-        return reversed(names)
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return[b for a,b in sorted(zip(heights,names), reverse = True)]
