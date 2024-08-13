1dc60170e72ff2fc87ab5e78ee7c7b0448d87a62
Shubh Sharma <146938449+Shubh15203@users.noreply.github.com>
2024-08-13 12:16:06 +0530

    Time: 3 ms (97.21%) | Memory: 10.1 MB (10.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mp;
-        int i=0;
-        int j=0;
-        for(i;i<s.length();i++){
-            if(mp.find(s[i]) != mp.end()){
-                mp[s[i]]++;
+        unordered_map<char, int> count;
+        
+        // Count the frequency of characters in string s
+        for (auto x : s) {
+            count[x]++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (auto x : t) {
+            count[x]--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (auto x : count) {
+            if (x.second != 0) {
+                return false;
             }
-            else mp[s[i]]=1;
         }
-        for(j;j<t.length();j++){
-            if(mp.find(t[j]) != mp.end()){
-                if(mp[t[j]]==0) return false;
-                mp[t[j]]--;
-            }
-            else return false;
-        }
-        for(int k=0;k<mp.size();k++){
-            if(mp[k]!=0) return false;
-        }
+        
         return true;
     }
 };
