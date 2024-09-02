48f6640a4f376aab55eaad2638ed0e28e80ffbe2
Shubh Sharma <146938449+Shubh15203@users.noreply.github.com>
2024-09-02 22:53:09 +0530

    Time: 108 ms (12.88%) | Memory: 77.1 MB (18.08%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -4,9 +4,10 @@ public:
       long long sum=0;
       for(int i=0;i<chalk.size();i++) sum+=chalk[i];
 
-      while(k>sum){
-        k-=sum;
-      }  
+    //   while(k>sum){
+    //     k-=sum;
+    //   }  
+    k%=sum;
       for(int i=0;i<chalk.size();i++){
         if(chalk[i]>k) return i;
         else k-=chalk[i];
