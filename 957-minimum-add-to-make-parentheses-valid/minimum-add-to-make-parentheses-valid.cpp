ec08b630cc0dad69ea64a6122053b143c4dc0313
Shubh Sharma <146938449+Shubh15203@users.noreply.github.com>
2024-10-09 22:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (55.34%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int extra=0;
-        stack<char> st; 
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                st.push(s[i]);
-            }
-            else if(s[i]==')' && !st.empty()){
-                st.pop();
-            }
-            else if(s[i]==')' && st.empty()){
-                extra++;
+        int open = 0, close = 0;
+        for (auto c : s) {
+            if (c == '(') {
+                open++;
+            } else {
+                if (open > 0)
+                    open--;
+                else
+                    close++;
             }
         }
-        return st.size()+extra;
+        return open + close;
     }
 };
