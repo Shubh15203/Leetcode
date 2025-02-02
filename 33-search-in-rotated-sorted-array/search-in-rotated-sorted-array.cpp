bfea0ca0471a8111abbe81d18244ae5e0b0e4ef6
Shubh Sharma <146938449+Shubh15203@users.noreply.github.com>
2025-02-02 22:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,35 +2,23 @@ class Solution {
 public:
     int search(vector<int>& arr, int target) {
         int n = arr.size();
-        int low = 0, high = n - 1;
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        //if mid points the target
-        if (arr[mid] == target) return mid;
-
-        //if left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            if (arr[low] <= target && target <= arr[mid]) {
-                //element exists:
-                high = mid - 1;
-            }
-            else {
-                //element does not exist:
-                low = mid + 1;
-            }
-        }
-        else { //if right part is sorted:
-            if (arr[mid] <= target && target <= arr[high]) {
-                //element exists:
-                low = mid + 1;
-            }
-            else {
-                //element does not exist:
-                high = mid - 1;
+        int low = 0, high = arr.size() - 1, mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] == target)
+                return mid;
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= target && target <= arr[mid]) {
+                    high = mid - 1;
+                } else
+                    low = mid + 1;
+            } else {
+                if (arr[mid] <= target && target <= arr[high]) {
+                    low = mid + 1;
+                } else
+                    high = mid - 1;
             }
         }
-    }
-    return -1;
+        return -1;
     }
 };
